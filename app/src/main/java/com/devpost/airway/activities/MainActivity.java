522fc030879092185c95a838ba8c6b97d322e16f
shaheenkdr <shaheenkader@outlook.com>
2016-10-19 01:55:18 -0700

    added phone calls

--- app/src/main/java/com/devpost/airway/activities/MainActivity.java
+++ app/src/main/java/com/devpost/airway/activities/MainActivity.java
@@ -1,7 +1,13 @@
 package com.devpost.airway.activities;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -52,18 +58,33 @@ public class MainActivity extends AppCompatActivity
     private Context mContext;
     private static int event_id;
     private static int response_code;
+    private Intent intent;
     private static boolean flag;
     private static final String id ="44e80599-c74c-4bc7-b30e-41730a6adfb2";
     private static final String sub_key ="3c12c7fd5d4c408ab856288e695ee5f7";
+    private static final int REQUEST_PHONE_CALL = 1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        finish();
-        Intent ss = new Intent(MainActivity.this,FoodActivity.class);
-        startActivity(ss);
+        intent = new Intent(Intent.ACTION_CALL, Uri.parse("tel:" + "+918511812660"));
+
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CALL_PHONE},REQUEST_PHONE_CALL);
+            }
+            else
+            {
+                startActivity(intent);
+            }
+        }
+        else
+        {
+            startActivity(intent);
+        }
+
 
         EventBus myEventBus = EventBus.getDefault();
         EventBus.getDefault().register(this);
@@ -109,6 +130,22 @@ public class MainActivity extends AppCompatActivity
             }
         });
 
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_PHONE_CALL: {
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    startActivity(intent);
+                }
+                else
+                {
+
+                }
+                return;
+            }
+        }
     }
 
     private  void doGetRequest(final String message)
