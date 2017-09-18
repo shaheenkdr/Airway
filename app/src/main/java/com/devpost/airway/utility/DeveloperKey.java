5e6ffaf5ad270621b8dd7d020437e5fa43883266
Shaheen A Kader <shaheenkader@outlook.com>
2017-09-18 17:19:45 +0530

    Update DeveloperKey.java

--- app/src/main/java/com/devpost/airway/utility/DeveloperKey.java
+++ app/src/main/java/com/devpost/airway/utility/DeveloperKey.java
@@ -3,6 +3,6 @@ package com.devpost.airway.utility;
 public class DeveloperKey {
 
 
-    public static final String DEVELOPER_KEY = "AIzaSyC5Em9j52BmZJvdcz3lRro6sbw_0W6sNGM";
+    public static final String DEVELOPER_KEY = "YOUR API KEY HERE";
 
 }
